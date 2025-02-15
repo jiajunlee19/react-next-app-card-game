6ccfb9403bffa03dc899de1bb538ab404c0aa612
Jia Jun Lee <lee19@live.cn>
2025-02-15 09:51:45 +0800

    [refactor] Move layout footer to component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,13 @@
 import { Toaster } from 'react-hot-toast'
 import Header from '@/app/_components/header'
+import Footer from '@/app/_components/footer'
 import '@/app/globals.css'
 import type { Metadata } from 'next'
 import AuthProvider from '@/app/_components/auth_provider'
 import { getServerSession } from "next-auth/next"
 import { options } from '@/app/_libs/nextAuth_options'
-import { GitHubIcon } from '@/app/_components/basic/icons'
 import { twMerge } from 'tailwind-merge'
 import ThemeContextProvider from '@/app/_context/theme-context'
-import Link from 'next/link'
 // import { Inter } from 'next/font/google'
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -48,19 +47,7 @@ export default async function RootLayout({
                 </main>
 
                 <footer className="fixed z-30 w-full h-14 bottom-0 lg:-ml-64 xl:-ml-72 bg-white dark:bg-zinc-900">
-                  <div className="absolute inset-x-0 bottom-full h-1 transition bg-zinc-900 dark:bg-white" />
-                  <div className="flex flex-col items-center justify-between max-sm:my-2 sm:flex-row sm:mx-4 sm:h-full">
-                    <p className="text-xs text-zinc-600 dark:text-zinc-400 whitespace-nowrap">© Copyright 2030. All rights reserved.</p>
-                    <div className="flex gap-2 items-center">
-                      <p className="text-xs text-zinc-600 dark:text-zinc-400 whitespace-nowrap">Developed by jiajunlee</p>
-                      <button className="hover:bg-zinc-900/50 dark:hover:bg-white/50">
-                        <Link href="/portfolio">
-                          <span className="sr-only">View Jia Jun Lee&apos;s Portfolio</span>
-                          <GitHubIcon className="h-5 w-5 fill-zinc-700 dark:fill-white" />
-                        </Link>
-                      </button>
-                    </div>
-                  </div>
+                  <Footer />
                 </footer>
               </div>
               <Toaster position="bottom-right" />
