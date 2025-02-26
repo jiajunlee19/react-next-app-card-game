e5cb556089d0f6843e93b899fc3a7b4a214373a6
Jia Jun Lee <lee19@live.cn>
2025-02-26 20:17:45 +0800

    [feat] Add 5 card selectors

--- app/(pages)/(card-game)/ngao/calculator/component.tsx
+++ app/(pages)/(card-game)/ngao/calculator/component.tsx
@@ -19,6 +19,13 @@ export default function NgaoCalculatorComponent({ digitValuePairs, initialCardCo
     const [card4, setCard4] = useState<keyof TDigitValuePairs | null>(null);
     const [card5, setCard5] = useState<keyof TDigitValuePairs | null>(null);
 
+    const cards = [
+        {cardNumber: "c1", card: card1, description: "Select Card 1"}, 
+        {cardNumber: "c2", card: card2, description: "Select Card 2"},
+        {cardNumber: "c3", card: card3, description: "Select Card 3"},
+        {cardNumber: "c4", card: card4, description: "Select Card 4"},
+        {cardNumber: "c5", card: card5, description: "Select Card 5"},
+    ];
 
     // Functions
 
@@ -32,19 +39,28 @@ export default function NgaoCalculatorComponent({ digitValuePairs, initialCardCo
 
         setCard1(null);
         setCard2(null);
+        setCard3(null);
+        setCard4(null);
+        setCard5(null);
     };
 
-    const handleCardSelect = (cardNumber: "c1" | "c2") => (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const handleCardSelect = (cardNumber: "c1" | "c2" | "c3" | "c4" | "c5") => (e: React.ChangeEvent<HTMLSelectElement>) => {
         const digit = Number(e.target.value) as unknown as keyof TDigitValuePairs;
 
         if (cardCounter[digit] > 0) {
             if (cardNumber === "c1") setCard1(digit);
             else if (cardNumber === "c2") setCard2(digit);
+            else if (cardNumber === "c3") setCard3(digit);
+            else if (cardNumber === "c4") setCard4(digit);
+            else if (cardNumber === "c5") setCard5(digit);
         }
 
         else {
             if (cardNumber === "c1") setCard1(null);
             else if (cardNumber === "c2") setCard2(null);
+            else if (cardNumber === "c3") setCard3(null);
+            else if (cardNumber === "c4") setCard4(null);
+            else if (cardNumber === "c5") setCard5(null);
         }
 
         return
@@ -56,7 +72,7 @@ export default function NgaoCalculatorComponent({ digitValuePairs, initialCardCo
             <p className="text">Select three cards to calculate your points on the first round. Select five cards to calculate your ox strength on the second round.</p>
             <button className="btn-primary w-fit" onClick={() => handleReset()}>Reset</button>
       
-            {[{cardNumber: "c1", card: card1, description: "Select Card 1"}, {cardNumber: "c2", card: card2, description: "Select Card 2"}].map((item) => {
+            {cards.map((item) => {
                 return (
                     <select className="w-fit" key={item.cardNumber} value={item.card || ""} onChange={handleCardSelect(item.cardNumber as "c1" | "c2")}>
                         <option value="">{item.description}</option>
