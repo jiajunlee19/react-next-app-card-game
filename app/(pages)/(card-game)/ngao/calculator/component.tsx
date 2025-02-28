108512ae63b70daefb77af323669e6b0706df545
Jia Jun Lee <lee19@live.cn>
2025-02-28 21:38:05 +0800

    [feat] Add selectors disable state handlers

--- app/(pages)/(card-game)/ngao/calculator/component.tsx
+++ app/(pages)/(card-game)/ngao/calculator/component.tsx
@@ -30,6 +30,24 @@ export default function NgaoCalculatorComponent({ digitValuePairs, initialCardCo
 
 
     // Functions
+    function canCalculateFirst() {
+        // if card 4 or 5 selected, return false
+        if (cardSelectors.slice(3, 5).some(cardSelector => cardSelector.card !== null)) return false;
+
+        // if all card 1/2/3 are disabled, return false
+        if (cardSelectors.slice(0, 3).every(cardSelector => cardSelector.disabled)) return false;
+
+        // if all card 1/2/3 are selected, return true
+        return cardSelectors.slice(0, 3).every(cardSelector => cardSelector.card !== null);
+    };
+
+    function canCalculateSecond() {
+        // if all card 1/2/3/4/5 are disabled, return false
+        if (cardSelectors.every(cardSelector => cardSelector.disabled)) return false;
+
+        // if all card 1/2/3/4/5 are selected, return true
+        return cardSelectors.every(cardSelector => cardSelector.card !== null);
+    };
 
 
     // Handlers
@@ -39,9 +57,13 @@ export default function NgaoCalculatorComponent({ digitValuePairs, initialCardCo
             return;
         }
 
-        setCardSelectors(prevCardSelectors => prevCardSelectors.map((cardSelector) => {
-            return {...cardSelector, card: null};
-        }));
+        setCardSelectors([
+            {cardNumber: "c1", card: null, description: "Select Card 1", disabled: false}, 
+            {cardNumber: "c2", card: null, description: "Select Card 2", disabled: false},
+            {cardNumber: "c3", card: null, description: "Select Card 3", disabled: false},
+            {cardNumber: "c4", card: null, description: "Select Card 4", disabled: true},
+            {cardNumber: "c5", card: null, description: "Select Card 5", disabled: true},
+        ]);
     };
 
     const handleCardSelect = (cardNumber: "c1" | "c2" | "c3" | "c4" | "c5") => (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -64,15 +86,31 @@ export default function NgaoCalculatorComponent({ digitValuePairs, initialCardCo
         return
     };
 
+    const handleCalculate = () => {
+        if (canCalculateFirst()) {
+            setCardSelectors(prevCardSelectors => prevCardSelectors.map((cardSelector) => {
+                if (cardSelector.cardNumber === "c1" || cardSelector.cardNumber === "c2" || cardSelector.cardNumber === "c3") return {...cardSelector, disabled: true}
+                else if (cardSelector.cardNumber === "c4" || cardSelector.cardNumber === "c5") return {...cardSelector, disabled: false};
+                return cardSelector
+            }));
+        }
+
+        if (canCalculateSecond()) {
+            setCardSelectors(prevCardSelectors => prevCardSelectors.map((cardSelector) => {
+                return {...cardSelector, disabled: true};
+            }));
+        }
+    };
+
 
     return (
         <div className="flex flex-col gap-8 my-4">
-            <p className="text">Select three cards to calculate your points on the first round. Select five cards to calculate your ox strength on the second round.</p>
-            <button className="btn-primary w-fit" onClick={() => handleReset()}>Reset</button>
-            <button className="btn-primary w-fit" onClick={() => handleReset()}>Calculate</button>
+            <p className="text">Select three cards to calculate your points on the first round. Select another 2 cards to calculate your ox strength on the second round.</p>
+            <button className="btn-primary w-fit" onClick={handleReset}>Reset</button>
+            <button className="btn-primary w-fit" onClick={handleCalculate} disabled={!canCalculateFirst() && !canCalculateSecond()}>Calculate</button>
             {cardSelectors.map((cardSelector) => {
                 return (
-                    <select className="w-fit" key={cardSelector.cardNumber} value={cardSelector.card || ""} onChange={handleCardSelect(cardSelector.cardNumber as "c1" | "c2" | "c3" | "c4" | "c5")}>
+                    <select className="w-fit" key={cardSelector.cardNumber} value={cardSelector.card || ""} onChange={handleCardSelect(cardSelector.cardNumber as "c1" | "c2" | "c3" | "c4" | "c5")} disabled={cardSelector.disabled}>
                         <option value="">{cardSelector.description}</option>
                         {Object.entries(cardCounter).map(([k, v]) => {
                             const digit = Number(k) as keyof typeof digitValuePairs;
