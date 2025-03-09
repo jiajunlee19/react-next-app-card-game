6bbf37f599560e60c8b55887b62179ec537f2a2f
Jia Jun Lee <lee19@live.cn>
2025-03-09 20:22:23 +0800

    [feat] When Ox strength formed with Ace of spades and a duke, you get "Ngao Dong Gu" = x5 payout

--- app/(pages)/(card-game)/ngao/calculator/component.tsx
+++ app/(pages)/(card-game)/ngao/calculator/component.tsx
@@ -74,7 +74,7 @@ export default function NgaoCalculatorComponent({ initialCardDeck, digitValuePai
             if ((cardSelector.cardNumber === "c1" || cardSelector.cardNumber === "c2" || cardSelector.cardNumber === "c3") && cardSelector.card?.digit) {
 
                 // J, Q, K with point of 10
-                if (cardSelector.card.digit === 11 || cardSelector.card.digit === 12 || cardSelector.card.digit === 13) points += 10;
+                if (cardSelector.card.digit > 10) points += 10;
 
                 // Others with point of their digit
                 else points += cardSelector.card.digit;
@@ -88,26 +88,115 @@ export default function NgaoCalculatorComponent({ initialCardDeck, digitValuePai
     };
 
     function calculateSecondResult() {
+
         let points = 0;
+        let oxChoices = [];
+        let oxStrengthChoices = [];
+        let finalOxCombinations = [];
+
+        // We will have 3 cards at the bottom and 2 cards at the top.
+        //  _ _     <--- this is your ox strength
+        // _ _ _    <--- this is your ox
+
+        // Sort the 5 cards so that we could easily arrange them later
+        let cards = [
+            cardSelectors[0].card, cardSelectors[1].card, cardSelectors[2].card, cardSelectors[3].card, cardSelectors[4].card
+        ].sort((a, b) => {
+            if (!a || !b) return 0;
+            if (a.digit === b.digit) {
+                const suitOrder = { "♦": 1, "♣": 2, "♥": 3, "♠": 4 };
+                return suitOrder[a.suit] - suitOrder[b.suit];
+            }
+            return a.digit - b.digit;
+        });
+
+        // Check if can form 3 cards with multiplier of 10, if yes then you haveOx, else no Ox
+        // Number of iteration = 5C3 = 5!/(3!2!) = 10, use 3 pointers method
+        const n = cards.length
+        if (n < 3) return;
+        for (let l=0; l<n-2; l++) {
+            for (let m=l+1; m<n-1; m++) {
+                for (let r=m+1; r<n; r++) {
+                    let [digit1, suit1] = [cardSelectors[l].card?.digit, cardSelectors[l].card?.suit];
+                    let [digit2, suit2] = [cardSelectors[m].card?.digit, cardSelectors[m].card?.suit];
+                    let [digit3, suit3] = [cardSelectors[r].card?.digit, cardSelectors[r].card?.suit];
+
+                    if (!digit1 || !digit2 || !digit3 || !suit1 || !suit2 || !suit3) return;
+
+                    // By default digit value is the digit, unless its J, Q, K
+                    let digitValue1 = digit1;
+                    let digitValue2 = digit2;
+                    let digitValue3 = digit3;
+
+                    // For J, Q, K, overwrite digit value to 10
+                    if (digit1 > 10) digitValue1 = 10;
+                    if (digit2 > 10) digitValue2 = 10;
+                    if (digit3 > 10) digitValue3 = 10;
+
+                    // If the sum is equal to multiplier of 10, add them into oxChoices and oxStrengthChoices accordingly
+                    if ((digitValue1 + digitValue2 + digitValue3) % 10 === 0) {
+                        oxChoices.push([
+                            { digit: digit1, suit: suit1 },
+                            { digit: digit2, suit: suit2 },
+                            { digit: digit3, suit: suit3 },
+                        ]);
+
+                        const remainingCards = cards.filter((_, index) => index !== l && index !== m && index !== r);
+                        oxStrengthChoices.push([
+                            {digit: remainingCards[0]?.digit, suit: remainingCards[0]?.suit},
+                            {digit: remainingCards[1]?.digit, suit: remainingCards[1]?.suit},
+                        ])
+                    }
+                }
+            }
+        };
+        
+        // If no ox, return the result as it is
+        if (oxChoices.length < 1) {
+            setResult(prevResult => `${prevResult}\nResult for Second Round = 0 (No Ox)`)
+            return;
+        }
+
+        // For each oxChoice, check which choice is giving out the best ox strength.
+        oxChoices.forEach((oxChoice, index) => {
+
+            // When Ox strength formed with Ace of spades and a duke, you get "Ngao Dong Gu" = x5 payout	
+            if (oxStrengthChoices[index].some(card => card.digit === 1 && card.suit === '♠') && oxStrengthChoices[index].some(card => [11, 12, 13].includes(card?.digit ?? 0))) {
+                points = 5;
+            }
+
+            finalOxCombinations.push({
+                oxCombination: [
+                    `${oxChoice[0].digit}${oxChoice[0].suit}`,
+                    `${oxChoice[1].digit}${oxChoice[1].suit}`,
+                    `${oxChoice[2].digit}${oxChoice[2].suit}`,
+                    `${oxStrengthChoices[index][0].digit}${oxStrengthChoices[index][0].suit}`,
+                    `${oxStrengthChoices[index][1].digit}${oxStrengthChoices[index][1].suit}`,
+                ], 
+                points: points,
+            });
+
+            // When Ox strength formed with Ace of non-spades and a duke, you get "Ngao Nen Gu" = x3 payout
+
+            // When Ox strength formed with two same value, you get "Double Ox" = x2 payout
+
+            // When Ox strength formed with no special ox and ones digit of the sum = 0 or 10, you get "Single Ox 10" = x2 payout
+
+            // When Ox strength formed with no special ox, strength = the ones digit of the sum ranged from 1 to 9, you get "Single Ox" = x1 payout
+
+            // When there's no any three cards sum up to multiple of 10, you get "No Ox" = 0 payout
+
+        });
 
         // When all 5 cards are dukes J/Q/K, you get "Din Shi Gai" or "Five Dukes" = x7 payout
         if (cardSelectors.every(cardSelector => ["J", "Q", "K"].includes(cardSelector.card?.value ?? ""))) {
             points = 7;
         }
 
-        // When Ox strength formed with Ace of spades and a duke, you get "Ngao Dong Gu" = x5 payout	
 
-        // When Ox strength formed with Ace of non-spades and a duke, you get "Ngao Nen Gu" = x3 payout
 
-        // When Ox strength formed with two same value, you get "Double Ox" = x2 payout
 
-        // When Ox strength formed with no special ox and ones digit of the sum = 0 or 10, you get "Single Ox 10" = x2 payout
-
-        // When Ox strength formed with no special ox, strength = the ones digit of the sum ranged from 1 to 9, you get "Single Ox" = x1 payout
-
-        // When there's no any three cards sum up to multiple of 10, you get "No Ox" = 0 payout
-
-        setResult(prevResult => `${prevResult}\nResult for Second Round = ${points}`)
+        setResult(prevResult => `${prevResult}\nResult for Second Round = ${points}\n${JSON.stringify(oxChoices)}`)
     };
 
 
