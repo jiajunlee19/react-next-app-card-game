a3aa8e5986c69699acd07dc5804c4ebbfe864844
Jia Jun Lee <lee19@live.cn>
2025-06-15 20:21:40 +0800

    [refactor] Change portfolio to github link

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
                     <div className="flex gap-2 items-center">
                     <p className="text-xs text-zinc-600 dark:text-zinc-400 whitespace-nowrap">Developed by jiajunlee</p>
                     <button className="hover:bg-zinc-900/50 dark:hover:bg-white/50">
-                        <Link href="/portfolio">
+                        <Link href="https://github.com/jiajunlee19">
                             <span className="sr-only">View Jia Jun Lee&apos;s Portfolio</span>
                             <GitHubIcon className="h-5 w-5 fill-zinc-700 dark:fill-white" />
                         </Link>
